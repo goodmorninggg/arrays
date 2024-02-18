b6eebea0a8c5674c9d16602b117f5d7f3cc4cb1a
goodmorninggg <soorajsingh6712@gmail.com>
2024-02-18 13:50:34 +0530

    changes in callback

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-// Callbacks
-function loadScript(src, callback) {
+function loadscript(src,callback) {
     var script = document.createElement("script");
-    script.src = src;
+    console.log("script without src",script )
+    script.src=src;
+    console.log("script with src",script )
+    console.log(src);
     script.onload = function() {
-      console.log("Loaded script with SRC: " + src)
-      callback(null, src);
+        console.log("loaded script with src: " + src);
+        callback(null, src);
     }
     script.onerror = function() {
-      console.log("Error loading script with SRC: " + src);
-      callback(new Error("Src got some error"))
+        console.log("error script with src: " + src);
+        callback(new Error("src got some error"));
     }
     document.body.appendChild(script);
-  }
-  
-  function hello(error, src) {
-    if (error) {
-      console.log(error)
-      return
+
+}
+function hello(src){
+    if(error) {
+        console.log(error)
+        return
     }
-    alert('Hello World!' + src);
-  }
-  
-  
-  function goodmorning(error, src) {
-    
-    if (error) {
-      console.log(error)
-      sendEmergencyMessageToCeo();
-      return
+    alert("heool world" + src);
+}
+function goodmorning(src){
+    if(error) {
+        console.log(error)
+        return
     }
-    alert('Good morning' + src);
-  }
-  
-  loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js", goodmorning) 
+    alert("goodmorning" + src);
+}
+loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",goodmorning)
